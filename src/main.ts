eba731f98833a54361db303ac4f0da3482047877
Florian Van Belleghem <fvanbelleghem@icloud.com>
2025-02-05 16:36:24 +0100

    feat: draw a square

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import shader from "./shader.wgsl?raw"
 const canvas = document.querySelector("canvas")!;
 
 if (!navigator.gpu) {
@@ -18,6 +19,57 @@ context?.configure({
   format: canvasFormat,
 });
 
+// prettier-ignore
+const vertices = new Float32Array([
+  //   X,    Y,
+    -0.8, -0.8, // Triangle 1 (Blue)
+     0.8, -0.8,
+     0.8,  0.8,
+  
+    -0.8, -0.8, // Triangle 2 (Red)
+     0.8,  0.8,
+    -0.8,  0.8,
+  ]);
+
+const vertexBuffer = device.createBuffer({
+  label: "Cell vertices",
+  size: vertices.byteLength,
+  usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+})
+
+device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
+
+const vertexBufferLayout = {
+  arrayStride: 8,
+  attributes: [{
+    format: "float32x2",
+    offset: 0,
+    shaderLocation: 0, // Position, see vertex shader
+  }],
+};
+
+const cellShaderModule = device.createShaderModule({
+  label: "Cell shader",
+  code: shader,
+});
+
+const cellPipeline = device.createRenderPipeline({
+  label: "Cell pipeline",
+  layout: "auto",
+  vertex: {
+    module: cellShaderModule,
+    entryPoint: "vertexMain",
+    buffers: [vertexBufferLayout]
+  },
+  fragment: {
+    module: cellShaderModule,
+    entryPoint: "fragmentMain",
+    targets: [{
+      format: canvasFormat
+    }]
+  }
+});
+
 const encoder = device.createCommandEncoder();
 const pass = encoder.beginRenderPass({
   colorAttachments: [
@@ -29,6 +81,8 @@ const pass = encoder.beginRenderPass({
     },
   ],
 });
-
+pass.setPipeline(cellPipeline);
+pass.setVertexBuffer(0, vertexBuffer);
+pass.draw(vertices.length / 2); // 6 vertices
 pass.end();
 device.queue.submit([encoder.finish()]);
